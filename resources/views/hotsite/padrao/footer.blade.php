7fd2533bf18bf482aed64ad080288633e0aff3ab
root <root@mukkz.taira>
2018-09-04 22:36:02 -0300

    Footer ok

--- resources/views/hotsite/padrao/footer.blade.php
+++ resources/views/hotsite/padrao/footer.blade.php
@@ -78,34 +78,3 @@
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('../assets/js/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-
-<!-- Bootstrap 3.3.7 -->
-<script src="{{asset('../assets/js/bootstrap.min.js')}}"></script>
-<!-- Morris.js charts -->
-<script src="{{asset('../assets/js/raphael.min.js')}}"></script>
-<script src="{{asset('../assets/js/morris.min.js')}}"></script>
-<!-- Sparkline -->
-<script src="{{asset('../assets/js/jquery.sparkline.min.js')}}"></script>
-<!-- jvectormap -->
-<script src="{{asset('../assets/js/jquery-jvectormap-1.2.2.min.js')}}"></script>
-
-<script src="{{asset('../assets/js/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{asset('../assets/js/jquery.knob.min.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{asset('../assets/js/moment.min.js')}}"></script>
-<script src="{{asset('../assets/js/daterangepicker.js')}}"></script>
-<!-- datepicker -->
-<script src="{{asset('../assets/js/bootstrap-datepicker.min.js')}}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{asset('../assets/js/bootstrap3-wysihtml5.all.min.js')}}"></script>
-<!-- Slimscroll -->
-<script src="{{asset('../assets/js/jquery.slimscroll.min.js')}}"></script>
-<!-- FastClick -->
-<script src="{{asset('../assets/js/fastclick.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('../assets/js/adminlte.min.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('../assets/js/dashboard.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{asset('../assets/js/demo.js')}}"></script>
--- resources/views/sistema/core.blade.php
+++ resources/views/sistema/core.blade.php
@@ -1,40 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Mapa de Reservas</title>
 
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+@extends('sistema.padraoSistema.head')
 
-    <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="{{asset('../assets/css/bootstrap.min.css')}}">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('../assets/css/font-awesome.min.css')}}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="{{asset('../assets/css/ionicons.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('../assets/css/AdminLTE.min.css')}}">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{asset('../assets/css/_all-skins.min.css')}}">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="{{asset('../assets/css/morris.css')}}">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="{{asset('../assets/css/jquery-jvectormap.css')}}">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="{{asset('../assets/css/bootstrap-datepicker.min.css')}}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{asset('../assets/css/daterangepicker.css')}}">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="{{asset('../assets/css/bootstrap3-wysihtml5.min.css')}}">
+@section('titulo', 'Mapa de Reserva')
 
 
 
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
@@ -409,73 +379,21 @@
                 <button type="button" class="btn btn-primary btn-sm" data-widget="remove"><i class="fa fa-times"></i>
                 </button>
               </div>
-              <!-- /. tools -->
             </div>
-            <!-- /.box-header -->
             <div class="box-body no-padding">
               <!--The calendar -->
               <div id="calendar" style="width: 100%"></div>
             </div>
-            <!-- /.box-body -->
 
           </div>
-          <!-- /.box -->
-
         </section>
-        <!-- right col -->
       </div>
-      <!-- /.row (main row) -->
-
-
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
-  </footer>
 
 
-<!-- jQuery 3 -->
-<script src="{{asset('../assets/js/jquery.min.js')}}"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{asset('../assets/js/jquery-ui.min.js')}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.7 -->
-<script src="{{asset('../assets/js/bootstrap.min.js')}}"></script>
-<!-- Morris.js charts -->
-<script src="{{asset('../assets/js/raphael.min.js')}}"></script>
-<script src="{{asset('../assets/js/morris.min.js')}}"></script>
-<!-- Sparkline -->
-<script src="{{asset('../assets/js/jquery.sparkline.min.js')}}"></script>
-<!-- jvectormap -->
-<script src="{{asset('../assets/js/jquery-jvectormap-1.2.2.min.js')}}"></script>
-<script src="{{asset('../assets/js/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{asset('../assets/js/jquery.knob.min.js')}}"></script>
-<!-- daterangepicker -->
-<script src="{{asset('../assets/js/moment.min.js')}}"></script>
-<script src="{{asset('../assets/js/daterangepicker.js')}}"></script>
-<!-- datepicker -->
-<script src="{{asset('../assets/js/bootstrap-datepicker.min.js')}}"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{asset('../assets/js/bootstrap3-wysihtml5.all.min.js')}}"></script>
-<!-- Slimscroll -->
-<script src="{{asset('../assets/js/jquery.slimscroll.min.js')}}"></script>
-<!-- FastClick -->
-<script src="{{asset('../assets/js/fastclick.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('../assets/js/adminlte.min.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('../assets/js/dashboard.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{asset('../assets/js/demo.js')}}"></script>
+@include('sistema.padraoSistema.footer')
+
+
 
 
 <!-- Modal de registro de Cliente -->
