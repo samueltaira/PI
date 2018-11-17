d7f5bf327bd9815afcbba0865e1c4a9f27027b76
Samuel Costa <samuel.tcosta@edu.univali.br>
2018-11-17 18:27:13 -0200

    kok

--- resources/views/hotsite/padrao/header.blade.php
+++ resources/views/hotsite/padrao/header.blade.php
@@ -4,10 +4,10 @@
             <h1><a href="{{url('/')}}"><div class="titulou">C o n t r o l <span>H o t e l</span></div></a></h1>
         </div>
 
-        <div class="form-inline" style="line-height: 1.2; margin-left: 70%">
-            <a style="margin-right: 3%" href="{{url('/')}}">Home</a>
-            <a href="{{url('politica')}}">Política de Privacidade</a>
-            <a style="margin-left: 3%"href="{{url('login')}}"><button class="login100">Login</button></a>
+        <div class="navbar-right">
+            <a class="m-r-20" href="{{url('/')}}">Home</a>
+            <a class="m-r-20" href="{{url('politica')}}">Política de Privacidade</a>
+            <a class="m-r-20" href="{{url('login')}}"><button class="btn-sm btn-info">LOGIN</button></a>
         </div>
     </nav>
 
