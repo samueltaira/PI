bc827f15bf3dc8f944d33a273a1a6211f2be1c45
Samuel Costa <31329394+samueltaira@users.noreply.github.com>
2019-06-07 15:32:35 -0300

    Update head.blade.php

--- resources/views/hotsite/padrao/head.blade.php
+++ resources/views/hotsite/padrao/head.blade.php
@@ -9,12 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Fonts e icones do login -->
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{sset('../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
 
     <!-- Css geral -->
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('../assets/css/util.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('../assets/css/main.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('../assets/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('../assets/css/main.css')}}">
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
@@ -25,11 +25,11 @@
 
 
     <!-- CSS header -->
-    <link rel="stylesheet" href="{{secure_asset('../assets/css/demo.css')}}">
-    <link rel="stylesheet" href="{{secure_asset('../assets/css/header-fixed.css')}}">
+    <link rel="stylesheet" href="{{asset('../assets/css/demo.css')}}">
+    <link rel="stylesheet" href="{{asset('../assets/css/header-fixed.css')}}">
     <link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
 
     <!-- CSS footer-->
-    <link rel="stylesheet" href="{{secure_asset('../assets/css/footer.css')}}">
+    <link rel="stylesheet" href="{{asset('../assets/css/footer.css')}}">
 
 </head>
