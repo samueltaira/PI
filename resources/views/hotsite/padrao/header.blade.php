16607bad93438eb2741acaef330691b534a35c86
Samuel Costa <31329394+samueltaira@users.noreply.github.com>
2019-06-07 16:22:54 -0300

    Update header.blade.php

--- resources/views/hotsite/padrao/header.blade.php
+++ resources/views/hotsite/padrao/header.blade.php
@@ -1,13 +1,13 @@
 <header class="header-fixed " style="position: absolute; top: 0; left: 0;">
     <nav class="header-limiter">
         <div>
-            <h1><a href="{{url('/', true)}}"><div class="titulou">C o n t r o l <span>H o t e l</span></div></a></h1>
+            <h1><a href="{{url('/')}}"><div class="titulou">C o n t r o l <span>H o t e l</span></div></a></h1>
         </div>
 
         <div class="navbar-right">
-            <a class="m-r-20" href="{{url('/', true)}}">Home</a>
-            <a class="m-r-20" href="{{url('politica', true)}}">Política de Privacidade</a>
-            <a class="m-r-20" href="{{url('login', true)}}"><button class="btn-sm btn-info">LOGIN</button></a>
+            <a class="m-r-20" href="{{url('/')}}">Home</a>
+            <a class="m-r-20" href="{{url('politica')}}">Política de Privacidade</a>
+            <a class="m-r-20" href="{{url('login')}}"><button class="btn-sm btn-info">LOGIN</button></a>
         </div>
     </nav>
 
